516aab9d35bad6b6294fef31e8df3b3dac8559c6
wojtekWOW <wojciechstachyra.wow@gmail.com>
2021-11-16 23:30:30 +0000

    program layout

---
+++ matrix.rb
@@ -0,0 +1,14 @@
+
+puts "MatrixA: "
+printMatrix(matrixA)
+
+puts
+
+puts "MatrixB: "
+printMatrix(matrixB)
+
+puts
+
+puts "Matrix multiplication"
+newMatrix = multiplyMatrix(matrixA, matrixB)
+printMatrix(newMatrix)
