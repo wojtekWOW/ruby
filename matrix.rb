91b9313899a2441dfcdc5bfe91d091b64f1b66fe
wojtekWOW <wojciechstachyra.wow@gmail.com>
2021-11-17 13:06:51 +0000

    add function to sum two matrixes

--- matrix.rb
+++ matrix.rb
@@ -16,7 +16,21 @@ def printMatrix(matrix)
   end
 end
 
+def sumMatrix(matrix1, matrix2)
+  if matrix1.length==matrix2.length && matrix1[0].length==matrix2[0].length
+    matrixC=Array.new(matrix1.length){Array.new(matrix1[0].length){0}}
 
+    for i in 0..matrixC.length-1
+      for j in 0..matrixC[0].length-1
+        matrixC[i][j]= matrix1[i][j]+matrix2[i][j]
+      end
+    end
+  else
+    puts "You can sum only matrixes with the same size "
+    matrixC=[]
+  end
+  return matrixC
+end
 
 puts "MatrixA: "
 printMatrix(matrixA)
