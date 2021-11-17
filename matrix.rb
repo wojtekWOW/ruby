dfffef7297f3ee62f584056a6b9cbd73754f0ef6
wojtekWOW <wojciechstachyra.wow@gmail.com>
2021-11-17 09:01:46 +0000

    added empty matrix.rb

--- matrix.rb
+++ matrix.rb
@@ -1,14 +1,1 @@
 
-puts "MatrixA: "
-printMatrix(matrixA)
-
-puts
-
-puts "MatrixB: "
-printMatrix(matrixB)
-
-puts
-
-puts "Matrix multiplication"
-newMatrix = multiplyMatrix(matrixA, matrixB)
-printMatrix(newMatrix)
